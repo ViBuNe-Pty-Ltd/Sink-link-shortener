a6979b8036381ea8bc70b4cf5aad8a0123337e89
The ViBuNe <95163874+vibune@users.noreply.github.com>
2025-06-10 21:27:42 +1000

    Update Header.vue

--- app/components/layouts/Header.vue
+++ app/components/layouts/Header.vue
@@ -48,7 +48,7 @@ const { title, github } = useAppConfig()
                 class="flex items-center justify-center w-8 h-8 text-white bg-gray-900 rounded-full"
               >
                 <img
-                  src="/sink.png"
+                  src="https://vibune.com/wp-content/uploads/2024/11/applogo.png"
                   :alt="title"
                   class="w-full h-full rounded-full"
                 >

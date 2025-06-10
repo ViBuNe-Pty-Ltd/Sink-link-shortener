a433d3bccfc5d3fbefe5898c133dc6125178b027
The ViBuNe <95163874+vibune@users.noreply.github.com>
2025-06-10 21:12:11 +1000

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,13 +1,13 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  title: 'Ihaz.Link',
+  email: 'ihazlink@vibune.com',
+  github: 'https://github.com/ViBuNe-Pty-Ltd',
+  twitter: 'https://x.com/thevibune',
+  telegram: 'https://telegram.org',
+  mastodon: 'https://vibune.com/mastodon',
+  blog: 'https://vibune.com/news,
+  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare. Thanks to ccbikai for the original Sink project.',
+  image: 'https://vibune.com/wp-content/uploads/2023/04/spinner-1-1-1.svg',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
